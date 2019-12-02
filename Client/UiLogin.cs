e2cf30c3a4766e40a4c885cd908a44802a95d484
Thommelise <Thomsen91@outlook.com>
2019-12-02 11:21:16 +0100

    Opret bruger, Login Tileføjet email og pass til message

---
+++ Client/UiLogin.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Client
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+     
+
+        private void bntOpretBrugerSide(object sender, EventArgs e)
+        {
+            UiOpretbruger m = new UiOpretbruger();
+            m.Show();
+        }
+
+        private void bntLogin(object sender, EventArgs e)
+        {
+            //Tager teksten fra textbox message
+            string input = textBoxUsername.Text;
+            string input1 = textBoxPassword.Text;
+
+            //tømmer textbox
+            textBoxUsername.Clear();
+            textBoxPassword.Clear();
+            //Laver json objekt
+            Message JsonObject = new Message();
+            JsonObject.Password = input1;
+            JsonObject.Username = input;
+            JsonObject.Function = "Login";
+
+            ServerFunctions ServerFunctions = new ServerFunctions();
+            ServerFunctions.AddToQueue(JsonObject);
+        }
+    }
+}
---
+++ Client/UiOpretbruger.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Client
+{
+    public partial class UiOpretbruger : Form, 
+    {
+        public UiOpretbruger()
+        {
+            InitializeComponent();
+        }
+
+        private void BntOpretBruger_Click(object sender, EventArgs e)
+        {
+            //Tager teksten fra textbox message
+            string input = textBoxEmail.Text;
+            string input1 = textBoxUsername.Text;
+            string input2 = textBoxPassword.Text;
+            
+            //tømmer textbox
+            textBoxEmail.Clear();
+            textBoxUsername.Clear();
+            textBoxPassword.Clear();
+
+            //Laver json objekt
+            Message JsonObject = new Message();
+            JsonObject.Password = input2;
+            JsonObject.Username = input1;
+            JsonObject.Email = input;
+            JsonObject.Function = "Opret bruger";
+
+            ServerFunctions ServerFunctions = new ServerFunctions();
+            ServerFunctions.AddToQueue(JsonObject);
+        }
+    }
+}
